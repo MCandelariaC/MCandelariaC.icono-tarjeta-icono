72b3ecd2cf5395fb59caac7b5b7f9c3e9d7eafb0
MCandelariaC <123111940+MCandelariaC@users.noreply.github.com>
2023-02-09 19:57:37 -0300

    Add files via upload

---
+++ Curso Front End Ticmas/function.js
@@ -0,0 +1,11 @@
+// function volver al inicio
+const desplazarArriba = document.querySelector("#desplazarse-hacia-arriba");
+
+desplazarArriba.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
